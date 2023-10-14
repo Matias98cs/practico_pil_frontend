43bf24a2e5a92b35a6a2c2ce2629b0ad745c2a98
Juan-imo21 <131096024+Juan-imo21@users.noreply.github.com>
2023-10-14 15:26:35 -0300

    Home(casi listo)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,70 @@
-import React from "react";
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon,
+  MDBCollapse,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
-const Header = () => {
+export default function Header() {
+  const [showBasic, setShowBasic] = useState(false);
+
   return (
-    <>
-      <header>
-        <h2>Administracion de datos academicos</h2>
-        <div>
-          <button type="button">Personas</button>
-          <button type="button">Cerrar Session</button>
+    <header>
+      <MDBNavbar expand='lg' light bgColor='black'>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            onClick={() => setShowBasic(!showBasic)}
+            aria-controls='navbarExample01'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <MDBIcon fas icon='bars' />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showBasic}>
+            <MDBNavbarNav right className='mb-2 mb-lg-0'>
+              <MDBNavbarItem active>
+              <div className="">
+              <button className="btn btn-primary">
+               inicio
+              </button>
+              </div>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'></MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'></MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+              <div className="">
+              <button className="btn btn-primary">
+               Cerrar Sesion
+              </button>
+              </div>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <div
+        className= "p-5 text-center bg-image headercolor container-fluid"
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'>Administracion de datos academicos</h1>
+              <h4 className='mb-3'></h4>
+            </div>
+          </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
-};
-export default Header;
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,61 @@
 import useAuth from "../../hooks/useAuth";
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Home = () => {
+export default function Home() {
   const { auth } = useAuth();
-
   return (
-    <div>
-      <h1>HOME ACADEMICO</h1>
-      <h4>Bienvenido {auth.username}</h4>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        excepturi itaque inventore, recusandae iure distinctio, officia mollitia
-        voluptas totam sapiente enim! Libero ab soluta ipsa excepturi cum nihil
-        impedit fugit!
-      </p>
-    </div>
-  );
-};
+    <div className="homeimage container-fluid">
+        <Container className="justify-content-center ">
+          <Row>
+            <Col md={4}>
+              <Button variant="primary" className="boton-con-imagen">
+                <div className="contenedor-texto mt-auto ">
+                Personas
+              </div>
+              </Button>
+            </Col>
+            <Col md={4}>
+              <Button variant="secondary" className="boton-con-imagen2">
+                <div className="contenedor-texto">
+                Carreras
+                </div>
+              </Button>
+            </Col>
+            <Col md={4}>
+              <Button variant="success" className="boton-con-imagen3">
+                <div className="contenedor-texto">
+                Universidades
+                </div>
+                </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4}>
+              <Button variant="danger" className="boton-con-imagen4">
+                <div className="contenedor-texto">
+                Facultades
+                </div>
+                </Button>
+            </Col>
+            <Col md={4}>
+              <Button variant="warning" className="boton-con-imagen5">
+                <div className="contenedor-texto">
+                Campus
+                </div>
+                </Button>
+            </Col>
+            <Col md={4}>
+              <Button variant="info" className="boton-con-imagen6">
+                <div className="contenedor-texto">
+                Programas
+                </div>
+                </Button>
+            </Col>
+          </Row>
+        </Container>
+        </div>
+      );
+    }
 
-export default Home;
