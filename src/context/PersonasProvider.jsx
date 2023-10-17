77ef370ddabacf8fbe111402e19c52b7162cf0b7
mcampos <mcampos@dlr.com.ar>
2023-10-17 15:09:12 -0300

    personas borrar, crear

--- src/context/PersonasProvider.jsx
+++ src/context/PersonasProvider.jsx
@@ -4,26 +4,27 @@ import clienteAxios from "../config/ClienteAxios";
 const PersonasContext = createContext();
 
 const PersonasProvider = ({ children }) => {
-  const personasPorPagina = 10;
+  // const personasPorPagina = 10;
   const [personas, setPersonas] = useState([]);
   const [paginado, setPaginado] = useState();
   const [paginaActual, setPaginaActual] = useState(1);
+  const [insertarPersona, setInsertarPersona] = useState({});
+  const [actualizar, setActualizar] = useState(false);
   useEffect(() => {
-    const algonose = async () => {
+    const obtenerPersonas = async () => {
       try {
         const response = await fetch(
           `http://127.0.0.1:9001/personas?page=${paginaActual}`
         );
         const resultado = await response.json();
-        console.log(resultado);
         setPaginado(resultado.total_paginas);
         setPersonas(resultado.personas);
       } catch (error) {
         console.log(error);
       }
     };
-    algonose();
-  }, [paginaActual]);
+    obtenerPersonas();
+  }, [paginaActual, actualizar]);
 
   const paginaSiguiente = () => {
     if (paginaActual < paginado) {
@@ -37,6 +38,34 @@ const PersonasProvider = ({ children }) => {
     }
   };
 
+  const postPersona = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:9001/personas/crear", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(insertarPersona),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const eliminarPersona = async (id) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:9001/personas/${id}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      const resultado = await response.json();
+      setActualizar(!actualizar);
+      console.log(resultado);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <PersonasContext.Provider
       value={{
@@ -44,6 +73,9 @@ const PersonasProvider = ({ children }) => {
         paginado,
         paginaSiguiente,
         paginaAnterior,
+        setInsertarPersona,
+        postPersona,
+        eliminarPersona,
       }}
     >
       {children}
